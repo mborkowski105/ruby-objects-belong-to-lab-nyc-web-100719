f7dcad045fb24da96c9156487ed6bca25486e48f
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-10 13:07:00 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,7 @@
+class Artist 
+  attr_reader :name
+  
+  def initialize
+    @name
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+class Song
+  attr_reader :name, :artist
+  
+  def initializer
+    @name
+    @artist
+  end
+  
+  
+end
